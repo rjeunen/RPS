b08b42e90b763d025054dfea67bf3fd36fcd02a1
Robby Jeunen <robbyjeunen@gmail.com>
2025-01-21 22:42:26 +0100

    added a function to play just 1 round and update the score

--- app.js
+++ app.js
@@ -48,3 +48,37 @@ function getHumanChoice(){
  * To test function:
  * console.log(getHumanChoice());
  */
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == computerChoice){
+        console.log(`It's a draw, you both choose ${humanChoice}!`);
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock" 
+        || humanChoice == "rock" && computerChoice == "scissors" 
+        || humanChoice == "scissors" && computerChoice == "paper"){
+            console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+    }
+    else if(humanChoice == "paper" && computerChoice == "scissors"
+        || humanChoice == "rock" && computerChoice == "paper"
+        || humanChoice == "scissors" && computerChoice == "rock"){
+            console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+    }
+    else{
+        console.log(`You did not enter a valid choice!`);
+    }
+}
+
+//variables for the humanChoice and computerChoice
+const humanSelection =  getHumanChoice();
+const computerSelection = getComputerChoice();
+
+//play a round
+playRound(humanSelection, computerSelection);
+
+/**
+ * To test scores:
+ * console.log(humanScore);
+ * console.log(computerScore);
+ */
