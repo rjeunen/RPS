3ba3a5861fc4c578f5165ca80311c26b90fd366b
Robby Jeunen <robbyjeunen@gmail.com>
2025-01-21 21:53:16 +0100

    added function to get a random computer choice

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hello");
+function getComputerChoice(){
+    let computerSelection = Math.floor(Math.random()*3) + 1;
+
+    if(computerSelection == 1){
+        return `rock`;
+    }
+    else if(computerSelection == 2){
+        return `paper`;
+    }
+    else if(computerSelection == 3){
+        return `scissors`;
+    }
+    else{
+        return `error - something went wrong!`;
+    }
+}
+/*
+to test function
+getComputerChoice();
+console.log(computerSelection);
+console.log(getComputerChoice());
+*/
