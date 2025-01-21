e070f335d0ac108fd8111f3ea46568906c77aba1
Robby Jeunen <robbyjeunen@gmail.com>
2025-01-21 22:11:39 +0100

    added function to get a player choice and handled error handling for the function

--- app.js
+++ app.js
@@ -14,9 +14,33 @@ function getComputerChoice(){
         return `error - something went wrong!`;
     }
 }
-/*
-to test function
-getComputerChoice();
-console.log(computerSelection);
-console.log(getComputerChoice());
-*/
+
+/**
+ * to test function
+ * getComputerChoice();
+ * console.log(computerSelection);
+ * console.log(getComputerChoice());
+ */
+
+function getHumanChoice(){
+    let playerChoice = prompt("Make a choice: Rock, Paper or Scissors:");
+    playerChoice = playerChoice.toLowerCase();
+
+    if(playerChoice == "rock"){
+        return "rock";
+    }
+    else if(playerChoice == "paper"){
+        return "paper";
+    }
+    else if(playerChoice == "scissors"){
+        return "scissors";
+    }
+    else{
+        return "error - something went wrong";
+    }
+}
+
+/**
+ * To test function:
+ * console.log(getHumanChoice());
+ */
